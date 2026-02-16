a44c6f78c6161159fccc37b00e56c093feb05c54
Ahmed Ali <ahmedyarr1212@gmail.com>
2026-02-17 00:04:59 +0500

    gsap day 04  improved

--- gsap_04/script.js
+++ gsap_04/script.js
@@ -1,15 +1,32 @@
 let main = document.querySelector("#main")
 let cursor = document.querySelector("#cursor")
+let image = document.querySelector("#image")
 
 main.addEventListener("mousemove", (dd) => {
-    console.log("Event Clicked.");
-    console.log("dd:", dd.x, " ", dd.y);
+    // console.log("Event Clicked.");
+    // console.log("dd:", dd.x, " ", dd.y);
 
     gsap.to(cursor, {
         x: dd.x,
         y: dd.y,
         duration: 1,
         ease: "back.out",
+    })
+
+})
+image.addEventListener("mouseenter", (dd) => {
+    console.log("Image Event Clicked.");
+
+    gsap.to(cursor, {
+        scale: 4
+    })
+
+})
+image.addEventListener("mouseleave", (dd) => {
+    console.log("Image Event Clicked.");
+
+    gsap.to(cursor, {
+        scale: 1
     })
 
 })

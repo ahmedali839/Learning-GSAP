5bb6c81c3f02e3a99678dd4362bc6b227a09bb80
ahmedali839 <ahmedyarr1212@gmail.com>
2026-04-26 23:56:05 +0500

    useGSAP

---
+++ gsap-10(react-1)/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "./App.css";
+
+const App = () => {
+  const gsapRef = useRef();
+
+  useGSAP(
+    () => {
+      gsap.from(".box", {
+        // gsap.from(gsapRef.current, {
+        // x: 1500,
+        scale: 0,
+        duration: 1,
+        // delay: 1,
+        opacity: 0,
+        rotate: 360,
+      });
+    },
+    { scope: ".container" },
+  );
+
+  return (
+    <main>
+      <div className="container">
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
+
+      <div className="kuch">
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
+    </main>
+  );
+};
+
+export default App;

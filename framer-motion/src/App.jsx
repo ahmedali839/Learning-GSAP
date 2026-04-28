a6458144b68fcadca641a1ed72aae4531f36dc91
ahmedali839 <ahmedyarr1212@gmail.com>
2026-04-28 19:43:23 +0500

    framer=motion

---
+++ framer-motion/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { motion } from "motion/react";
+
+const App = () => {
+  return (
+    <div>
+      <motion.div
+        drag
+        animate={{
+          x: [0, 800, 800, 0, 0],
+          y: [0, 0, 300, 300, 0],
+          rotate: [0, 360, 0, -360, 0],
+        }}
+        transition={{
+          duration: 2,
+          delay: 1,
+        }}
+        className="box"
+      >
+        Hey, Yar
+      </motion.div>
+    </div>
+  );
+};
+
+export default App;

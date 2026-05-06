33bcbf5e6fec7841e17fe3bcdc96aa0ed75bd6f9
Ahmed Yar <141534802+ahmedali839@users.noreply.github.com>
2026-05-06 22:27:04 +0500

    Updated script.js gsap

--- gsap_12/script.js
+++ gsap_12/script.js
@@ -4,7 +4,7 @@ const page2 = document.querySelector("#page2");
 const cardStrip = document.querySelector("#page2 .card");
 const getScrollDistance = () => cardStrip.scrollWidth - page2.clientWidth;
 
-gsap.to(cardStrip, { // to control gsap specific scrolling for limited area
+gsap.to(cardStrip, { // // to control gsap specific scrolling for limited area
   x: () => -getScrollDistance(),
   ease: "none",
   scrollTrigger: {
